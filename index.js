31dd7b13652399241305ef4a1ad860aaa81d44c2
riskezwn <enriquegarciagasc@gmail.com>
2022-03-27 16:26:51 +0200

    Configuración básica de servidor con express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+const express = require("express");
+
+// Create express server
+const app = express();
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+// Listen request
+app.listen(4001, () => {
+  console.log(`Server running on ${4001}`);
+});

e4d6b50207a8c26f8bf3c2184bf64d26e1ab6b3d
riskezwn <enriquegarciagasc@gmail.com>
2022-03-27 17:04:41 +0200

    Creación endpoints de login, register y renew token

--- routes/auth.js
+++ routes/auth.js
@@ -6,10 +6,10 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    ok: true,
-  });
-});
+const { createUser, loginUser, renewToken } = require("../controllers/auth");
+
+router.post("/new", createUser);
+router.post("/", loginUser);
+router.post("/renew", renewToken);
 
 module.exports = router;

dbfd9b520787d54d19dc42b065a39f50d3f80f43
riskezwn <enriquegarciagasc@gmail.com>
2022-03-27 16:51:50 +0200

    Añadir rutas de autenticación

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ const app = express();
 app.use(express.static("public"));
 
 // Routes
-/* app.get("/", (req, res) => {
-  res.json({
-    ok: true,
-  });
-}); */
+app.use('/api/auth', require('./routes/auth'))
+// TODO: CRUD // events
 
 // Listen request
 app.listen(process.env.PORT, () => {
---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+/* 
+    Auth routes 
+    host + /api/auth
+*/
+
+const { Router } = require("express");
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+module.exports = router;

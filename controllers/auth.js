508575ca52418e3629a563b0bf51f9aa5b1054a4
riskezwn <enriquegarciagasc@gmail.com>
2022-03-29 13:59:12 +0200

    Validación de correo electrónico en uso y encriptación de contraseña

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,31 @@
 const { response } = require("express");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 const createUser = async (req, res = response) => {
+  const { email, password } = req.body;
+
   try {
-    const user = new User(req.body);
+    let user = await User.findOne({ email: email });
+
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El correo electrónico ya está en uso"
+      });
+    };
+
+    user = new User(req.body);
+    // Encrypt password
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
+
     await user.save();
 
     res.status(201).json({
       ok: true,
-      msg: "register",
+      uid: user.id,
+      name: user.name
     });
   } catch (error) {
     res.status(500).json({

0a2dbd8fffc38cc810f356b786ea970ffebaf012
riskezwn <enriquegarciagasc@gmail.com>
2022-03-31 14:36:10 +0200

    Validación campos en la petición de crear evento

---
+++ helpers/isDate.js
@@ -0,0 +1,13 @@
+const moment = require("moment");
+
+const isDate = (value, { req, location, path }) => {
+  if (!value) {
+    return false;
+  }
+  const date = moment(value);
+  if (date.isValid()) {
+    return true;
+  }
+};
+
+module.exports = { isDate };
--- routes/events.js
+++ routes/events.js
@@ -5,7 +5,10 @@
 
 const { Router } = require("express");
 const router = Router();
+const { check } = require("express-validator");
 
+const { isDate } = require("../helpers/isDate");
+const { validateField } = require("../middlewares/validate-field");
 const { validateJWT } = require("../middlewares/validate-jwt");
 const { getEvents, createEvent, updateEvent, deleteEvent } = require("../controllers/events");
 
@@ -15,7 +18,15 @@ router.use(validateJWT);
 // get events
 router.get("/", getEvents);
 // create new event
-router.post("/", createEvent);
+router.post(
+  "/",
+  [
+    check("title", "Title is required").notEmpty(),
+    check("start", "Start date is required").custom(isDate),
+    check("end", "End date is required").custom(isDate),
+    validateField
+  ],
+  createEvent);
 // update event by id
 router.put("/:id", updateEvent);
 // delete event

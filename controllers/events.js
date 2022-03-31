429ebc0b0f31ad04e76adc0928d122da0d455d15
riskezwn <enriquegarciagasc@gmail.com>
2022-03-31 15:06:55 +0200

    Obtener eventos de la base de datos

--- controllers/events.js
+++ controllers/events.js
@@ -1,10 +1,11 @@
 const { response } = require("express");
 const Event = require("../models/Event");
 
-const getEvents = (req, res = response) => {
+const getEvents = async(req, res = response) => {
+  const events = await Event.find().populate("user", "name");
   res.json({
     ok: true,
-    msg: "getEvents",
+    events
   });
 };
 const createEvent = async(req, res = response) => {

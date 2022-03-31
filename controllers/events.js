9f22a91ccf0f6c3cadaef445e187049355a89839
riskezwn <enriquegarciagasc@gmail.com>
2022-03-31 15:40:44 +0200

    Eliminar evento de la base de datos

--- controllers/events.js
+++ controllers/events.js
@@ -33,7 +33,7 @@ const updateEvent = async(req, res = response) => {
     const uid = req.uid;
 
     if (!event) {
-      res.status(404).json({
+      return res.status(404).json({
         ok: false,
         msg: "El evento no existe"
       });
@@ -51,23 +51,48 @@ const updateEvent = async(req, res = response) => {
 
     const eventUpdated = await Event.findByIdAndUpdate(eventId, newEvent, { new: true });
 
-    res.json({
+    return res.json({
       ok: true,
       event: eventUpdated
     });
   } catch (error) {
     console.log(error);
-    res.status(500).json({
+    return res.status(500).json({
       ok: false,
       msg: "Hable con el administrador"
     });
   }
 };
-const deleteEvent = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: "deleteEvent",
-  });
+const deleteEvent = async(req, res = response) => {
+  const eventId = req.params.id;
+  const uid = req.uid;
+  try {
+    const event = await Event.findById(eventId);
+
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: "El evento no existe"
+      });
+    }
+    if (event.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No tienes permiso para eliminar este evento"
+      });
+    }
+    await Event.findByIdAndRemove(eventId);
+
+    return res.json({
+      ok: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador"
+    });
+  }
 };
 
 module.exports = {
